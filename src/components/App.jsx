a6840cc525eae9919994d5bc20e68b921606bacd
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2022-08-08 20:18:19 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+
+export class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  handleClick = (key) => {
+    this.setState(prevState => ({
+        [key]: prevState[key] + 1,
+    }))
+  }
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((total, item) => total + item, 0);
+  }
+
+
+  render() {
+    const total = this.countTotalFeedback();
+
+    // в.1
+    // const { good, neutral, bad } = this.state;
+    // const countTotalFeedback = good + neutral + bad;
+    
+    return (
+      <div>
+        <h2>Please live feadback</h2>
+
+        {Object.keys(this.state).map(key => (
+          <button key={key} onClick={() => this.handleClick(key)}>{key}</button>
+        ))}
+        
+        <h2>Statistics</h2>
+
+          <p>Good: {this.state.good}</p>
+          <p>Neutral: {this.state.neutral}</p>
+          <p>Bad: {this.state.bad}</p>
+          <p>Total: {total}</p>
+
+      </div>
+    )
+  }
+}
